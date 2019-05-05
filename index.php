20e33902e8f71f25e48e0179cabff757d3de4556
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-05-05 17:50:18 +0300

    wowo

--- index.php
+++ index.php
@@ -388,7 +388,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="mail/script.js"></script>
+    <script src="mail.js"></script>
     <script src="Start-js.js"></script>
     <script src="preloader.js"></script>
   </body>
--- mail/mail.js
+++ mail/mail.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "../mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
 			alert("Thank you!");
