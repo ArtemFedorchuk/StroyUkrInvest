d3580287c521604d895fc383797ed82f6bacccec
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-05-05 19:23:05 +0300

    wow

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
               <div class="container">
                 <div class="row header__h1">
                   <div class="col-12 text-center">
-                    <span class="header__text mt-1">Добро пожаловать в компанию</span><br>
+                    <span class="header__text mt-1">Добро пожаловать в нашу компанию</span><br>
                     <span class="h1 animated zoomIn">Stroy Ukr Invest</span><br>
                     <span class="header__text"></span><br>
                     <a href="#" class="header__btn">Заказать звонок</a>
@@ -379,16 +379,19 @@
 
       <!-- php form script -->
 
-     
     
     <!-- Optional JavaScript -->
   
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!--     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ -->    
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="mail.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+    <script src="type-text.js"></script>
     <script src="Start-js.js"></script>
     <script src="preloader.js"></script>
   </body>
---
+++ type-text.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+  	let typed = new Typed(".header__text", {
+	  strings: ["Привет друзья", "Мы будем рады помочь Вам!","Если будут вопросы обращайтесь!"],
+	  startDelay: 2500,
+	  typeSpeed: 80,
+	  backSpeed: 30,
+	  loop: true,
+	  smartBackspace: true // Default value
+	});
+});
